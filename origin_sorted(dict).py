ea51510fe4d32977abf73b5b53996a56f829c13a
maxadm <maksimkor96@icloud.com>
2024-04-30 09:41:26 +0300

    добавлено извелечение в origin еще из текстового файла

--- origin_sorted(dict).py
+++ origin_sorted(dict).py
@@ -1,32 +1,57 @@
 import csv
 
+if input():  # Если 1: True - подается .csv, Если 0: False - подается .txt
+    flag = True
+    file_old = 'data/imported/file1.csv'
+else:  
+    flag = False
+    file_old = 'data/imported/status_ok.txt'
 
-# если True, то только с именами, False - все строки
-def importable(file_old='data/imported/file1.csv', names=True) -> list:
+# если names True, то только с именами, False - все строки
+def importable(file_old, names=False, flag=False) -> list:
     '''
     Функция достает из csv файла эмаил и имя
     Если подается names=True, то возвращает только те у которых указано имя
     Если False все подряд
     '''
     with open(file_old, 'r', encoding='utf-8') as file:
-        file_reader = csv.reader(file, delimiter=';')
         res_dict = {}  # словаьр под выходные значения
         c = 0  # cчетчик собранных значений
-        for i in file_reader:  # итарация по обекту из csv файла
-            if not c:
-                continue
-            else:
-                if names:  # если отбираем только с заполненными именами
-                    if i[4] != '""':
+        print(flag)
+
+        if flag:  # если .csv
+            file_reader = csv.reader(file, delimiter=';')
+            for i in file_reader:  # итарация по обекту из csv файла
+                if not c:
+                    #print(i)
+                    c += 1
+                    continue
+                else:
+                    if names:  # если отбираем только с заполненными именами
+                        if i[4] != '""':
+                            res_dict[i[3]] = i[4]
+                    else:  # если все подряд
                         res_dict[i[3]] = i[4]
-                else:  # если все подряд
-                    res_dict[i[3]] = i[4]
-            c += 1  # прибавляет к счетчику
+                    c += 1  # прибавляет к счетчику
+
+        else:  # если .txt
+            file_reader = file.readlines()  # достает из файла
+            for st in file_reader:  # итерирует по списку строк из файла
+                s = st.strip().split(';')  # преобразует строку в список посредствам разделителя ;
+                if len(s) == 2:  # если строка не пустая
+                    res_dict[s[0]] = s[1]
+                    c += 1  # прибавляет к счетчику2
+
+
+
         print(c, len(res_dict))
         return res_dict
+        
+            
 
 
-def writer_list(res_list=importable(names=False), file_name='data/file_new.txt') -> None:
+
+def writer_list(res_list=importable(file_old), file_name='data/file_new.txt') -> None:
     '''
     Функция записывает в файл file_name значения res_list
     По дефолту то что функция importable достала из csv файла
